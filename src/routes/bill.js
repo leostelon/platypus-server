b26385baff346924aceed490b1eb64164a33e242
leostelon <53221136+leostelon@users.noreply.github.com>
2023-12-09 17:58:49 +0530

    populate payment

--- src/routes/bill.js
+++ src/routes/bill.js
@@ -4,6 +4,7 @@ const { Bill } = require("../models/bill");
 router.post("/", async (req, res) => {
     try {
         const bill = await new Bill({ payment: req.body.payment }).save()
+        await bill.populate("payment")
         return res.send(bill)
     } catch (error) {
         res.status(500).send({ message: error.message });
